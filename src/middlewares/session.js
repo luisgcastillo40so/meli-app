749ef3f7460cccf735e46c46dd4d6d01df0fb212
Pablo Peña <gmlpubli00@gmail.com>
2022-10-10 21:09:44 -0300

    Feat: change errors returned to users and fix some bugs

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -5,19 +5,17 @@ const { userModel } = require("../models")
 const authMiddleware = async (req, res, next) => {
   try {
     if (!req.headers.authorization) {
-      handleThrowHttpError(res, 401, "NOT TOKEN")
+      handleThrowHttpError(res, 401, { code: 01, message: "NOT TOKEN" })
       return
     }
     const token = req.headers.authorization.split(" ").pop()
     const dataToken = verifyToken(token)
-    if (!dataToken || !dataToken.userId) {
-      handleThrowHttpError(res, 403, "INVALID TOKEN")
+    if (!dataToken || !dataToken.user["_id"]) {
+      handleThrowHttpError(res, 403, { code: 02, message: "Invalid token" })
       return
     }
-    const user = await userModel.findById(dataToken.userId)
-
-    !user && handleThrowHttpError(res, 403, "INVALID TOKEN")
-
+    const user = await userModel.findById(dataToken.user["_id"]).select("+password")
+    !user && handleThrowHttpError(res, 403, { code: 01, message: "Invalid token" })
     req.user = user
     next()
   } catch (err) {
