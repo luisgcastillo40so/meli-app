a6246e6654121368b5f5d953e8e746531e48f1d5
Pablo Peña <gmlpubli00@gmail.com>
2022-10-17 11:38:23 -0300

    Feat: configure multer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-
 const app = express()
 
 app.use(cors())
 app.use(express.static("public"))
 app.use(express.json())
+const multer = require("multer")
+
+const storage = multer.diskStorage({
+  destination: './temp',
+  filename: function (req, file, callback) {
+    callback(null, `${+ new Date()}${file.originalname}`);
+  }
+});
+
+const upload = multer({ storage: storage })
+app.use(upload.any())
 
 // Setting
 app.set("port", process.env.PORT || 8080)
