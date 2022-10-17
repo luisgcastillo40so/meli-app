92575e5b44c102b5624bf96b6567c5a8383b756c
Pablo Peña <gmlpubli00@gmail.com>
2022-10-17 11:40:38 -0300

    Feat: improve verification

--- src/middlewares/validators/user.js
+++ src/middlewares/validators/user.js
@@ -1,17 +1,65 @@
-const { check } = require("express-validator")
+const { body, oneOf } = require("express-validator")
 const validationResult = require("../../utils/handleValidator")
 
 const validateUpdateUser = [
-  check("email").bail().optional().isEmail(),
-  check("password").bail().optional().isLength({ min: 6 }),
-  check("store-name").bail().optional().isLength({ min: 3, max: 50 }),
-  check("settings").bail().optional().isObject(),
-  check("settings.backendUri").optional().isURL(),
-  check("settings.customersEndpoint").optional().isString().matches(/^[a-z]+(\/[a-z]+)*$/gi),
-  check("settings.suppliersEndpoint").optional().isString().matches(/^[a-z]+(\/[a-z]+)*$/gi),
-  check("settings.supportEndpoint").optional().isString().matches(/^[a-z]+(\/[a-z]+)*$/gi),
-  check("settings.ordersEndpoint").optional().isString().matches(/^[a-z]+(\/[a-z]+)*$/gi),
-  check("settings.productsEndpoint").optional().isString().matches(/^[a-z]+(\/[a-z]+)*$/gi),
+  body("email")
+    .isEmail(),
+  oneOf([
+    body("newPassword")
+      .isStrongPassword({
+        minLength: 6,
+        minLowercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+      })
+      .custom((value, { req }) => {
+        if (value === req.body.confirmPassword) return true
+        else throw new Error("the passwords are different")
+      }),
+    body("newPassword").custom((value, { req }) => {
+      if (value === "" && req.body.confirmPassword === "") return true
+      else throw new Error("the passwords are different")
+    })
+  ], "invalid password"),
+  body("storeName")
+    .exists()
+    .isLength({ min: 3, max: 50 }),
+  oneOf([
+    body("backendUri").isEmpty(),
+    body("backendUri").isURL()
+  ], "invalid base url"),
+  oneOf([
+    body("customersEndpoint").exists({
+      checkFalsy: true
+    }).matches(/^[a-z]+(\/[a-z]+)*$/gi),
+    body("customersEndpoint").isEmpty()
+  ], "invalid customersEndpoint"),
+  oneOf([
+    body("suppliersEndpoint").exists({
+      checkFalsy: true
+    }).matches(/^[a-z]+(\/[a-z]+)*$/gi),
+    body("suppliersEndpoint").isEmpty()
+  ], "invalid suppliersEndpoint"),
+  oneOf([
+    body("supportEndpoint").exists({
+      checkFalsy: true
+    }).matches(/^[a-z]+(\/[a-z]+)*$/gi),
+    body("supportEndpoint").isEmpty()
+  ], "invalid supportEndpoint"),
+  oneOf([
+    body("ordersEndpoint").exists({
+      checkFalsy: true
+    }).matches(/^[a-z]+(\/[a-z]+)*$/gi),
+    body("ordersEndpoint").isEmpty()
+  ], "invalid ordersEndpoint"),
+  oneOf([
+    body("customersEndpoint").exists({
+      checkFalsy: true
+    }).matches(/^[a-z]+(\/[a-z]+)*$/gi),
+    body("customersEndpoint").isEmpty()
+  ], "invalid customersEndpoint"),
+  body("address")
+    .isString(),
   validationResult,
 ]
 
